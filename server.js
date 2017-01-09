635e3b1b3a862a4d43b8e16adbf0917070c244f2
CdrXander <xackhackman@gmail.com>
2017-01-08 22:35:23 -0700

    Undoing a stupid mistake with heroku...

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var reviewNode 	= require('./node_controllers/reviewNode.js');
 
 //Connect to DB
 var conn = massive.connectSync({
-	connectionString:process.env.DATABASE_URL;
+	connectionString:"postgres://postgres:" + config.pglogin + "@localhost/cinema_node"
 });
 
 app.set('db',conn);
